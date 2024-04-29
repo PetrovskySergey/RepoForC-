9f5fe3ed6213b54f30c93a19a872cbd75b34327f
Sergey Petrovsky <sgp-61@mail.ru>
2024-04-29 11:50:56 +0300

    Initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке 
+// от M до N. Использовать рекурсию, не использовать циклы.
+
+void FromMToN(int m, int n)
+{
+if (m > n)
+{
+FromMToN(m - 1, n);
+System.Console.Write(m + " ");
+}
+else if (n > m)
+{
+FromMToN(m, n - 1);
+System.Console.Write(n + " ");
+}
+else System.Console.Write(m + " ");
+}
+FromMToN(4, 9);
---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца.
+//  Использовать рекурсию, не использовать циклы.
+
+
+void OutputOfArrayElementsInReverseOrder(int[] array, int i = 0)
+{
+    if (i < array.Length)
+    {
+        OutputOfArrayElementsInReverseOrder(array, i + 1);
+        Console.Write(array[i] + " ");
+    }
+}
+int[] array = { 3, 8, 1, 4, 13, 25 };
+OutputOfArrayElementsInReverseOrder(array);
